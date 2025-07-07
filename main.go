84cc6959fc3ae771588b389c69a835e746358163
shiponcs <abdulmatincuetcse17@gmail.com>
2025-07-07 12:49:22 +0600

    Improve error loggin
    Signed-off-by: shiponcs <abdulmatincuetcse17@gmail.com>

--- main.go
+++ main.go
@@ -15,14 +15,23 @@ func main() {
 		log.Fatal("DISCORD_BOT_TOKEN environment variable is required")
 	}
 
+	err := startBot(token)
+	if err != nil {
+		log.Fatalf("Failed to start bot: %v", err)
+	}
+
+	fmt.Println("Exiting the App")
+}
+
+func startBot(token string) error {
 	discordObj, err := discord.NewDiscord(token, discordgo.IntentsAll, true)
 	if err != nil {
-		fmt.Printf("ERROR NewDiscord %v\n", err)
-		return
+		log.Printf("Error creating Discord session: %v", err)
+		return err
 	}
 
 	discordObj.AddHandler(discord.MessageHandler)
 	discordObj.Init() // blocking call
 
-	fmt.Println("Exiting the App")
+	return nil
 }
